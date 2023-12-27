1dc20129bbcc6384b1ec7c288ad40c3473ee9bbc
keshab3736 <s373690@students.cdu.edu.au>
2023-12-27 18:41:57 +0930

    Create Brand Name Generator Project Day-1.py
    Day1: Brand Name Generator Project

---
+++ Brand Name Generator Project Day-1.py
@@ -0,0 +1,8 @@
+print ("WELCOME TO THE BAND NAME GENERATOR🚀")
+city_name = input("What's city did you grew up in?:")
+pet_name = input("What's the name of your pet?:")
+print ("The BAND NAME GENERATOR COULD BE:" + city_name + " " +  pet_name)
+
+
+
+
